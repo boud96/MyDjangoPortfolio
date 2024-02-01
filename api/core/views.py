f1a8ee248f147e8059df64db289260e7d3365d19
Adam.Boudnik <adam.boudnik@olc.cz>
2024-02-01 23:02:54 +0100

    Some index.html work
    Played with the view little and added some variables.

--- api/core/views.py
+++ api/core/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views import View
 
-from core.models import PersonalInfo, Job, Education
+from core.models import PersonalInfo, Job, Education, Skill
 
 
 class IndexView(View):
     def get(self, request):
-        return render(request, "index.html")
+        person = PersonalInfo.objects.get(active=True)
+        person.age = (timezone.now().date() - person.date_of_birth).days // 365
+
+        jobs = Job.objects.all()
+        educations = Education.objects.all()
+        skills = Skill.objects.all()
+        context = {"person": person, "jobs": jobs, "educations": educations, "skills": skills}
+        return render(request, "index.html", context)
 
 
 class IndexTestView(View):  # TODO: This is just temp test
@@ -14,5 +22,6 @@ class IndexTestView(View):  # TODO: This is just temp test
         person = PersonalInfo.objects.all().first()
         jobs = Job.objects.all()
         educations = Education.objects.all()
-        context = {"person": person, "jobs": jobs, "educations": educations}
+        skills = Skill.objects.all()
+        context = {"person": person, "jobs": jobs, "educations": educations, "skills": skills}
         return render(request, "indextest.html", context)
