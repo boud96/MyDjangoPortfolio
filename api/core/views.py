94b4f7c41f62597bf1aff64cecfb7739a5464064
Adam.Boudnik <adam.boudnik@olc.cz>
2024-02-16 13:14:30 +0100

    heroku - core module?

--- api/core/views.py
+++ api/core/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.views import View
 
-from core.models import (
+from api.core.models import (
     PersonalInfo,
     Job,
     Education,
